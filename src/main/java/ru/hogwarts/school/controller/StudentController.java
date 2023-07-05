6db44b62a417f931ac3f3b702e6e43f8b0b1bc2e
mariyatarasenko88 <tmv1231@yandex.ru>
2023-07-05 22:52:20 +0300

    Homework3.5 is donne

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -1,21 +1,34 @@
 package ru.hogwarts.school.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.AvatarService;
 import ru.hogwarts.school.service.StudentService;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collection;
 
 @RestController
 @RequestMapping("student")
 public class StudentController {
     private final StudentService studentService;
+    private final AvatarService avatarService;
 
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, AvatarService avatarService) {
         this.studentService = studentService;
+        this.avatarService = avatarService;
     }
 
     @GetMapping("{id}")
@@ -23,6 +36,7 @@ public class StudentController {
         Student student = studentService.findStudent(id);
         return ResponseEntity.ok(student);
     }
+
     @GetMapping
     public ResponseEntity<Collection<Student>> getAllStudents() {
         Collection<Student> students = studentService.getAllStudents();
@@ -34,11 +48,13 @@ public class StudentController {
         Collection<Student> foundStudents = studentService.getStudentsByAge(age);
         return ResponseEntity.ok(foundStudents);
     }
+
     @GetMapping("ages")
     public ResponseEntity<Collection<Student>> getStudentsByMinMaxAge(@RequestParam Integer min, @RequestParam Integer max) {
         Collection<Student> foundStudents = studentService.getStudentByAgeBetween(min, max);
         return ResponseEntity.ok(foundStudents);
     }
+
     @GetMapping("{id}/faculty")
     public ResponseEntity<Faculty> getFacultyByStudent(@PathVariable Long id) {
         Faculty foundFaculty = studentService.getFacultyByStudent(id);
@@ -61,4 +77,42 @@ public class StudentController {
     public void deleteStudent(@PathVariable Long id) {
         studentService.deleteStudent(id);
     }
+    @PostMapping(value = "/{id}/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadAvatar(@PathVariable Long id, @RequestParam MultipartFile avatar) throws IOException {
+        if (avatar.getSize() >= 1024 * 300) {
+            return ResponseEntity.badRequest().body("File is too big");
+        }
+
+        avatarService.uploadAvatar(id, avatar);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "/{id}/avatar/preview")
+    public ResponseEntity<byte[]> downloadAvatar(@PathVariable Long id) {
+        Avatar avatar = avatarService.findAvatar(id);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(avatar.getMediaType()));
+        headers.setContentLength(avatar.getData().length);
+
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(avatar.getData());
+    }
+
+    @GetMapping(value = "/{id}/avatar")
+    public void downloadAvatar(@PathVariable Long id, HttpServletResponse response) throws IOException {
+        Avatar avatar = avatarService.findAvatar(id);
+
+        Path path = Path.of(avatar.getFilePath());
+
+        try (InputStream is = Files.newInputStream(path);
+             OutputStream os = response.getOutputStream()) {
+            response.setStatus(200);
+            response.setContentType(avatar.getMediaType());
+            response.setContentLength((int) avatar.getFileSize());
+            is.transferTo(os);
+        }
+    }
+
 }
+
+
